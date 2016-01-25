f3583dc8e203aeb8ef8d7dfb5a47c50fcc2e2fc7
Alexander Schofield <alschofield93@gmail.com>
2016-01-25 17:59:14 -0500

    adds more heroku

--- app.js
+++ app.js
@@ -216,6 +216,6 @@ app.get('/api/album/:id', function(req, res) {
 })
 
 // run server
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('yaaaaaaaas');
 })
